c8d0fed1891663db8c2b82a6345311a9d6ba3736
Chancharchula <95258867+chachillie@users.noreply.github.com>
2026-01-19 10:01:18 -0500

    Add Flycast-iOS release manifest

---
+++ flycast-ios.js
@@ -0,0 +1,26 @@
+{
+  "name": "Flycast-iOS26",
+  "identifier": "com.chachirie.source",
+  "apps": [
+    {
+      "beta": false,
+      "name": "Flycast",
+      "bundleIdentifier": "com.chachirie.FlycastiOS",
+      "developerName": "Chachirie",
+      "subtitle": "Dreamcast, Naomi and Atomiswave emulator.",
+      "version": "v1.0",
+      "versionDate": "2025-01-18",
+      "versionDescription": "iOS 26 JIT release",
+      "downloadURL": "https://github.com/chachillie/Flycast-iOS/releases/download/v1.0-beta.1/FlycastiOS.ipa",
+      "localizedDescription": "Flycast is a Dreamcast, Naomi, Naomi 2 and Atomiswave emulator.",
+      "iconURL": "https://github.com/chachillie/Flycast-iOS/raw/main/shell/linux/flycast.png",
+      "tintColor": "9b54fd",
+      "size": 8703180,
+      "screenshotURLs": [
+        "https://github.com/chachillie/Flycast-iOS/raw/main/shell/imgs/screenshot1.png",
+        "https://github.com/chachillie/Flycast-iOS/raw/main/shell/imgs/screenshot2.png",
+        "https://github.com/chachillie/Flycast-iOS/raw/main/shell/imgs/screenshot3.png"
+      ]
+    }
+  ]
+}
